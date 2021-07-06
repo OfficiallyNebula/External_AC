8bf8a19c106d4944052300ce5a6955892cecd64a
Ben H | Nebula <email@officialharris.co.uk>
2021-07-06 20:20:05 +0100

    Added GetProcId

--- External_AC/External_AC.cpp
+++ External_AC/External_AC.cpp
@@ -1,20 +1,24 @@
-// External_AC.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	//Get process ID Of target process
+	//GetModuleBaseAddress
+
+	//Get Handle to process (Using handle process)
+
+	// Resolve base address of the pointer chain
+
+	//Resolve our ammo pointer chain
+
+	// Read ammo value
+
+	//Write ammo value
+
+	//Read ammo value
+
+
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ External_AC/proc.cpp
@@ -0,0 +1,39 @@
+#include "proc.h"
+
+DWORD GetProcID(const wchar_t* procName) 
+{
+	DWORD procId = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnap != INVALID_HANDLE_VALUE)
+	{
+		PROCESSENTRY32 procEntry;
+		procEntry.dwSize = sizeof(procEntry);
+
+		if (Process32First(hSnap, &procEntry))
+		{
+			do
+			{
+				if (!_wcsicmp(procEntry.szExeFile, procName))
+				{
+
+				}
+
+
+			} while (Process32Next(hSnap, &procEntry));
+		}
+	}
+	CloseHandle(hSnap);
+	return procId;
+}
+
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
+{
+
+
+}
+
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int>) 
+{
+
+
+}
